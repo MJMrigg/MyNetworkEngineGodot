72cc2a2520a4728bf2b0e7058dd7c17f1472adf4
MJMrigg <riggmatthew20@outlook.com>
2026-03-20 18:56:37 -0400

    Better Synchronization
    Both Synchronizers use the same function, since they only emit it on the peer that they're targetting.

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -6,12 +6,11 @@ public partial class Character : CharacterBody2D
 	[Export] public float Speed = 300.0f;
 	[Export] public MultiplayerSynchronizer ClientSynchronizer;
 	
-	//Whether the synchronizers are synced
-	public bool ClientSynced;
-	public bool ServerSynced;
-	
 	//Input variables
 	public Vector2 InputedVelocity = Vector2.Zero;
+	
+	//Whether the snychronizer is ready
+	public bool IsSynced;
 	
 	public override void _EnterTree(){
 		//Give client authority to the client synchronizer
@@ -20,33 +19,25 @@ public partial class Character : CharacterBody2D
 	}
 	
 	public override void _Ready(){
-		//Don't do anything until the synchronizer has been set up
 		base._Ready();
 	}
 	
 	//Mark if the server synchronizer is synced
-	public void ServerIsSynced(){
-		ServerSynced = true;
-	}
-	
-	//Mark if the client synchronizer is synced
-	public void ClientIsSynced(){
-		ClientSynced = true;
+	public void Synchronized(){
+		IsSynced = true;
 	}
 
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
-		//Only process if all synchronizers are working
-		/*if(!ClientSynced || !ServerSynced){
+		if(!IsSynced){
 			return;
-		}*/
+		}
 		if(GenericCore.Instance.IsServer()){ //Server
 			//Calculate velocity using the velocity from the client and move the object
 			Velocity = InputedVelocity.Normalized()*Speed;
 			MoveAndSlide();
 		}else{ //Clients
-			GD.Print(ClientSynchronizer.GetMultiplayerAuthority());
 			if(GenericCore.Instance.IsLocal(ClientSynchronizer.GetMultiplayerAuthority())){ //Local Client
 				//Get inputed velocity from the clients
 				InputedVelocity = Input.GetVector("Left","Right","Up","Down");
